76674258e622bbae015366b641abe0f0b72a59ec
Wallys Ferreira <wallysfsilva@gmail.com>
2023-05-27 19:40:59 -0300

    renomear classe de testes para o repositorio

--- src/test/java/com/matchink/api/RepositoryUsuarioTests.java
+++ src/test/java/com/matchink/api/RepositoryUsuarioTests.java
@@ -5,10 +5,9 @@ import com.matchink.api.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @DataMongoTest
-public class RepositoryUsuarioClass {
+public class RepositoryUsuarioTests {
     @Autowired
     UsuarioRepository repo;
 

ab8127cc67072bfa074f3cfff671df41713bd1f1
Wallys Ferreira <wallysfsilva@gmail.com>
2023-05-28 13:11:27 -0300

    Adicionar classe JsonPatch

--- src/main/java/com/matchink/api/Controller/UsuarioController.java
+++ src/main/java/com/matchink/api/Controller/UsuarioController.java
@@ -1,8 +1,8 @@
 package com.matchink.api.Controller;
 
 import com.matchink.api.Model.Usuario;
+import com.matchink.api.JsonPatch;
 import com.matchink.api.Repository.UsuarioRepository;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +21,26 @@ public class UsuarioController {
     @GetMapping("/usuarios/{id}")
     Usuario retornarUmUsuario(@PathVariable String id) {
         return repository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException(id));
+    }
+
+    @PatchMapping("/usuarios/{id}")
+    Usuario atualizarCampo(@PathVariable String id, @RequestBody JsonPatch patch) {
+        Usuario usuario = repository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException(id));
+
+        switch(patch.getOp()) {
+            case "replace":
+                switch(patch.getPath()) {
+                    case "bio":
+                        usuario.setBio(patch.getValue());
+                        break;
+                    default:
+
+
+                }
+        }
+
+        repository.save(usuario);
+
+        return usuario;
     }
 }
